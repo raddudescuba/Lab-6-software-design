d0b89d98eb50e02ddf3e9f468de9115e6bef671b
tuf97138 <43321987+tuf97138@users.noreply.github.com>
2018-12-09 18:27:46 -0500

    Add files via upload

--- generateMatrix.c
+++ generateMatrix.c
@@ -2,14 +2,18 @@
 #include<math.h>
 #include<stdio.h>
 #include<time.h>
+
 int main(int argc, char* argv[]){
+    //if there are more than 3 arguments given
     if(argc>3){
         srand(time(NULL));
-        int numRow=atoi(argv[1]);
+        int numRow=atoi(argv[1]); 
         int numCol=atoi(argv[2]);
         FILE * fp;
-        fp=fopen(argv[3],"w");
+        
+        fp = fopen(argv[3],"w"); //creates a file. if one already exists, write over it. 
         fprintf(fp,"rows(%d) cols(%d)\n",numRow,numCol);
+        
         int i,j;
         for(i=0;i<numRow;i++){
             for(j=0;j<numCol;j++){
@@ -18,6 +22,9 @@ int main(int argc, char* argv[]){
             fprintf(fp,"\n");
         }
         fclose(fp);
+    }else{
+        printf("Missing arguments <rows> <cols> <filename>\n");
     }
+    
     return 0;
 }
